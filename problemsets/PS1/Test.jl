46139356af047fe8386aef4be8aeb4a807e4f798
mccarthyg16 <113550755+mccarthyg16@users.noreply.github.com>
2023-02-17 15:06:34 -0500

    Add files via upload

--- problemsets/PS1/Test.jl
+++ problemsets/PS1/Test.jl
@@ -3,3 +3,6 @@ include("Include.jl")
 
 # text the teaching team will check -
 plaintext = "Julia is awesome. The computer is my best friend, and it loves me. This is a test and only a test."
+
+d = encrypt(plaintext)
+test = decrypt(d)
